89d52393963b86549628a3861aaf4ff9c38359a4
mateuszPr <mateuszprzeliorz@outlook.com>
2018-03-31 00:12:33 +0200

    Add files via upload
    Basic models, HomeController, first View

---
+++ KolaNaukowe.API/KolaNaukowe.API/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using KolaNaukowe.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolaNaukowe.API.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        private IStudentResearchGroupData _studentResearchGroupData;
+
+        public HomeController(IStudentResearchGroupData studentResearchGroupData)
+        {
+            _studentResearchGroupData = studentResearchGroupData;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _studentResearchGroupData.GetAll();
+
+            return View(model);
+        }
+    }
+}
